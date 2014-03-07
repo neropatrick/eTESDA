36a17098e357480382221f2a10ecfae20edf330f
JMTee <janmarvint@gmail.com>
2014-03-07 15:56:34 +0800

    try
    try

---
+++ aboutus.php
@@ -0,0 +1,91 @@
+<div class="container" style="padding-bottom: 300px ">
+    
+    <div class="span12">
+        
+        <h2> <?php echo Asset::img('glyphicons/glyphicons_003_user.png', 'alt text' );?> About the Team</h2>
+        <p> We are a diverse team of I.T Professionals from the College of Computer Studies, De La Salle University.</p>
+        <hr/>
+          <div class="row" style="margin-top:10px">
+        <div class="span6"> 
+        <div class="media">
+        <div class="pull-left" href="#">
+          <img class="media-object"  src="http://m.c.lnkd.licdn.com/mpr/mpr/shrink_200_200/p/1/000/04b/346/0f14c71.jpg" width="64px" height="64px" data-src="holder.js/64x64">
+        </div >
+        <div class="media-body">
+            <h4 class="media-heading">Dr. Lloyd Espiritu </h4>
+          <p> 
+             <small><i>  Project Manager of eTESDA Learn </i></small>
+          </p>
+        </div>
+      </div> 
+      </div>
+        
+       
+        </div>
+        <hr>
+        <div class="row">
+        <div class="span6"> 
+        <div class="media">
+        <div class="pull-left" href="#">
+          <img class="media-object"  src="http://m.c.lnkd.licdn.com/media/p/1/005/01f/2c3/01c742c.jpg"  width="64" height="64" data-src="holder.js/64x64">
+        </div >
+        <div class="media-body">
+          <h4 class="media-heading">Gerome Louie D.Gutierrez</h4>
+          <p> 
+              <small><i> Database Manager</i></small>
+              
+          </p>
+        </div>
+      </div> 
+      </div>
+        
+        <div class="span6"> 
+        <div class="media">
+        <div class="pull-left" href="#">
+          <img class="media-object" src="http://m.c.lnkd.licdn.com/mpr/mpr/shrink_200_200/p/1/000/253/271/244e152.jpg" width="64" height="64" data-src="holder.js/64x64">
+        </div >
+        <div class="media-body">
+          <h4 class="media-heading">John Patrick L. Ocariz</h4>
+          <p> 
+               <small><i> Lead Quality Assurance </i></small>
+            
+          </p>
+        </div>
+      </div> 
+      </div>
+        </div>
+        
+          <div class="row" style="margin-top:10px">
+        <div class="span6"> 
+        <div class="media">
+        <div class="pull-left" href="#">
+          <img class="media-object" src="http://m.c.lnkd.licdn.com/media/p/5/000/2b1/25b/180879f.jpg" width="64px" height="64px" data-src="holder.js/64x64">
+        </div >
+        <div class="media-body">
+          <h4 class="media-heading">Jan Marvin O. Tee</h4>
+          <p> 
+             <small><i> Front-end Developer</i></small>
+          </p>
+        </div>
+      </div> 
+      </div>
+        
+        <div class="span6"> 
+        <div class="media">
+        <div class="pull-left" href="#">
+          <img class="media-object" src="http://m.c.lnkd.licdn.com/media/p/8/000/267/3f8/2d0fc4d.jpg" width="64" height="64" data-src="holder.js/64x64">
+        </div >
+        <div class="media-body">
+          <h4 class="media-heading">Nero Patrick B. Roque</h4>
+          <p> 
+              <small><i> Lead Developer</i></small>
+          </p>
+        </div>
+      </div> 
+      </div>
+        </div>
+          
+        
+    </div>
+    
+</div>
