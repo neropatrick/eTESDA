c2fdafefd73e0039051f2921c098dcca6cf7f8da
JMTee <janmarvint@gmail.com>
2014-03-07 16:50:28 +0800

    fancybox integration
    fancy box .js
    applied it to lesson picture

--- system/cms/logs/log-2014-03-07.php
+++ system/cms/logs/log-2014-03-07.php
@@ -63,3 +63,87 @@ ERROR - 2014-03-07 15:29:55 --> Query error: Unknown column 'bootstrap' in 'wher
             WHERE Type = 4 AND ParentID = (SELECT ParentID FROM default_syllabus WHERE HeirarchyID = bootstrap AND Type = 4) ORDER BY `order`
 ERROR - 2014-03-07 15:42:27 --> Query error: Unknown column 'bootstrap' in 'where clause' - Invalid query: SELECT HeirarchyID FROM default_syllabus
             WHERE Type = 4 AND ParentID = (SELECT ParentID FROM default_syllabus WHERE HeirarchyID = bootstrap AND Type = 4) ORDER BY `order`
+ERROR - 2014-03-07 16:33:48 --> Query error: Unknown column 'dummy' in 'where clause' - Invalid query: SELECT HeirarchyID FROM default_syllabus
+            WHERE Type = 4 AND ParentID = (SELECT ParentID FROM default_syllabus WHERE HeirarchyID = dummy AND Type = 4) ORDER BY `order`
+ERROR - 2014-03-07 16:33:48 --> Query error: Unknown column 'bootstrap' in 'where clause' - Invalid query: SELECT HeirarchyID FROM default_syllabus
+            WHERE Type = 4 AND ParentID = (SELECT ParentID FROM default_syllabus WHERE HeirarchyID = bootstrap AND Type = 4) ORDER BY `order`
+ERROR - 2014-03-07 16:34:04 --> Query error: Unknown column 'bootstrap' in 'where clause' - Invalid query: SELECT HeirarchyID FROM default_syllabus
+            WHERE Type = 4 AND ParentID = (SELECT ParentID FROM default_syllabus WHERE HeirarchyID = bootstrap AND Type = 4) ORDER BY `order`
+ERROR - 2014-03-07 16:34:06 --> Query error: Unknown column 'bootstrap' in 'where clause' - Invalid query: SELECT HeirarchyID FROM default_syllabus
+            WHERE Type = 4 AND ParentID = (SELECT ParentID FROM default_syllabus WHERE HeirarchyID = bootstrap AND Type = 4) ORDER BY `order`
+ERROR - 2014-03-07 16:35:27 --> Query error: Unknown column 'bootstrap' in 'where clause' - Invalid query: SELECT HeirarchyID FROM default_syllabus
+            WHERE Type = 4 AND ParentID = (SELECT ParentID FROM default_syllabus WHERE HeirarchyID = bootstrap AND Type = 4) ORDER BY `order`
+ERROR - 2014-03-07 16:35:31 --> Query error: Unknown column 'bootstrap' in 'where clause' - Invalid query: SELECT HeirarchyID FROM default_syllabus
+            WHERE Type = 4 AND ParentID = (SELECT ParentID FROM default_syllabus WHERE HeirarchyID = bootstrap AND Type = 4) ORDER BY `order`
+ERROR - 2014-03-07 16:35:35 --> Query error: Unknown column 'bootstrap' in 'where clause' - Invalid query: SELECT HeirarchyID FROM default_syllabus
+            WHERE Type = 4 AND ParentID = (SELECT ParentID FROM default_syllabus WHERE HeirarchyID = bootstrap AND Type = 4) ORDER BY `order`
+ERROR - 2014-03-07 16:37:31 --> Query error: Unknown column 'bootstrap' in 'where clause' - Invalid query: SELECT HeirarchyID FROM default_syllabus
+            WHERE Type = 4 AND ParentID = (SELECT ParentID FROM default_syllabus WHERE HeirarchyID = bootstrap AND Type = 4) ORDER BY `order`
+ERROR - 2014-03-07 16:37:34 --> Query error: Unknown column 'bootstrap' in 'where clause' - Invalid query: SELECT HeirarchyID FROM default_syllabus
+            WHERE Type = 4 AND ParentID = (SELECT ParentID FROM default_syllabus WHERE HeirarchyID = bootstrap AND Type = 4) ORDER BY `order`
+ERROR - 2014-03-07 16:38:02 --> Severity: Notice  --> Trying to get property of non-object C:\xampp\htdocs\eTESDA\addons\shared_addons\modules\syllabus\models\getModules.php 90
+ERROR - 2014-03-07 16:38:02 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near 'AND Type = 3)' at line 2 - Invalid query: SELECT HeirarchyID FROM default_syllabus 
+        WHERE Type = 2 AND ID = (SELECT ParentID FROM default_syllabus WHERE HeirarchyID =  AND Type = 3)
+ERROR - 2014-03-07 16:38:03 --> Query error: Unknown column 'bootstrap' in 'where clause' - Invalid query: SELECT HeirarchyID FROM default_syllabus
+            WHERE Type = 4 AND ParentID = (SELECT ParentID FROM default_syllabus WHERE HeirarchyID = bootstrap AND Type = 4) ORDER BY `order`
+ERROR - 2014-03-07 16:38:03 --> Severity: Notice  --> Trying to get property of non-object C:\xampp\htdocs\eTESDA\addons\shared_addons\modules\syllabus\models\getModules.php 90
+ERROR - 2014-03-07 16:38:03 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near 'AND Type = 3)' at line 2 - Invalid query: SELECT HeirarchyID FROM default_syllabus 
+        WHERE Type = 2 AND ID = (SELECT ParentID FROM default_syllabus WHERE HeirarchyID =  AND Type = 3)
+ERROR - 2014-03-07 16:38:04 --> Severity: Notice  --> Trying to get property of non-object C:\xampp\htdocs\eTESDA\addons\shared_addons\modules\syllabus\models\getModules.php 90
+ERROR - 2014-03-07 16:38:04 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near 'AND Type = 3)' at line 2 - Invalid query: SELECT HeirarchyID FROM default_syllabus 
+        WHERE Type = 2 AND ID = (SELECT ParentID FROM default_syllabus WHERE HeirarchyID =  AND Type = 3)
+ERROR - 2014-03-07 16:38:04 --> Severity: Notice  --> Trying to get property of non-object C:\xampp\htdocs\eTESDA\addons\shared_addons\modules\syllabus\models\getModules.php 90
+ERROR - 2014-03-07 16:38:05 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near 'AND Type = 3)' at line 2 - Invalid query: SELECT HeirarchyID FROM default_syllabus 
+        WHERE Type = 2 AND ID = (SELECT ParentID FROM default_syllabus WHERE HeirarchyID =  AND Type = 3)
+ERROR - 2014-03-07 16:38:08 --> Query error: Unknown column 'bootstrap' in 'where clause' - Invalid query: SELECT HeirarchyID FROM default_syllabus
+            WHERE Type = 4 AND ParentID = (SELECT ParentID FROM default_syllabus WHERE HeirarchyID = bootstrap AND Type = 4) ORDER BY `order`
+ERROR - 2014-03-07 16:38:09 --> Query error: Unknown column 'bootstrap' in 'where clause' - Invalid query: SELECT HeirarchyID FROM default_syllabus
+            WHERE Type = 4 AND ParentID = (SELECT ParentID FROM default_syllabus WHERE HeirarchyID = bootstrap AND Type = 4) ORDER BY `order`
+ERROR - 2014-03-07 16:39:06 --> Query error: Unknown column 'bootstrap' in 'where clause' - Invalid query: SELECT HeirarchyID FROM default_syllabus
+            WHERE Type = 4 AND ParentID = (SELECT ParentID FROM default_syllabus WHERE HeirarchyID = bootstrap AND Type = 4) ORDER BY `order`
+ERROR - 2014-03-07 09:39:07 --> Page Missing: eTESDA/addons/shared_addons/themes/bootstrap/img/dummy/SPECIALISTA.png
+ERROR - 2014-03-07 09:39:08 --> Page Missing: eTESDA/addons/shared_addons/themes/bootstrap/img/dummy/SPECIALISTA.png
+ERROR - 2014-03-07 09:39:10 --> Page Missing: eTESDA/addons/shared_addons/themes/bootstrap/img/dummy/bootstrap/ico/favicon.png
+ERROR - 2014-03-07 09:39:12 --> Page Missing: eTESDA/addons/shared_addons/themes/bootstrap/img/dummy/bootstrap/ico/favicon.png
+ERROR - 2014-03-07 09:39:12 --> Page Missing: eTESDA/addons/shared_addons/themes/bootstrap/img/dummy/bootstrap/ico/favicon.png
+ERROR - 2014-03-07 09:39:13 --> Page Missing: eTESDA/addons/shared_addons/themes/bootstrap/img/dummy/bootstrap/ico/favicon.png
+ERROR - 2014-03-07 09:39:13 --> Page Missing: eTESDA/addons/shared_addons/themes/bootstrap/img/dummy/bootstrap/ico/favicon.png
+ERROR - 2014-03-07 16:39:14 --> Query error: Unknown column 'bootstrap' in 'where clause' - Invalid query: SELECT HeirarchyID FROM default_syllabus
+            WHERE Type = 4 AND ParentID = (SELECT ParentID FROM default_syllabus WHERE HeirarchyID = bootstrap AND Type = 4) ORDER BY `order`
+ERROR - 2014-03-07 09:39:20 --> Page Missing: eTESDA/addons/shared_addons/themes/bootstrap/img/dummy/SPECIALISTA.png
+ERROR - 2014-03-07 16:39:39 --> Query error: Unknown column 'bootstrap' in 'where clause' - Invalid query: SELECT HeirarchyID FROM default_syllabus
+            WHERE Type = 4 AND ParentID = (SELECT ParentID FROM default_syllabus WHERE HeirarchyID = bootstrap AND Type = 4) ORDER BY `order`
+ERROR - 2014-03-07 16:39:45 --> Query error: Unknown column 'bootstrap' in 'where clause' - Invalid query: SELECT HeirarchyID FROM default_syllabus
+            WHERE Type = 4 AND ParentID = (SELECT ParentID FROM default_syllabus WHERE HeirarchyID = bootstrap AND Type = 4) ORDER BY `order`
+ERROR - 2014-03-07 16:39:50 --> Query error: Unknown column 'bootstrap' in 'where clause' - Invalid query: SELECT HeirarchyID FROM default_syllabus
+            WHERE Type = 4 AND ParentID = (SELECT ParentID FROM default_syllabus WHERE HeirarchyID = bootstrap AND Type = 4) ORDER BY `order`
+ERROR - 2014-03-07 16:41:23 --> Query error: Unknown column 'bootstrap' in 'where clause' - Invalid query: SELECT HeirarchyID FROM default_syllabus
+            WHERE Type = 4 AND ParentID = (SELECT ParentID FROM default_syllabus WHERE HeirarchyID = bootstrap AND Type = 4) ORDER BY `order`
+ERROR - 2014-03-07 16:41:24 --> Query error: Unknown column 'bootstrap' in 'where clause' - Invalid query: SELECT HeirarchyID FROM default_syllabus
+            WHERE Type = 4 AND ParentID = (SELECT ParentID FROM default_syllabus WHERE HeirarchyID = bootstrap AND Type = 4) ORDER BY `order`
+ERROR - 2014-03-07 16:41:27 --> Query error: Unknown column 'bootstrap' in 'where clause' - Invalid query: SELECT HeirarchyID FROM default_syllabus
+            WHERE Type = 4 AND ParentID = (SELECT ParentID FROM default_syllabus WHERE HeirarchyID = bootstrap AND Type = 4) ORDER BY `order`
+ERROR - 2014-03-07 16:43:25 --> Query error: Unknown column 'bootstrap' in 'where clause' - Invalid query: SELECT HeirarchyID FROM default_syllabus
+            WHERE Type = 4 AND ParentID = (SELECT ParentID FROM default_syllabus WHERE HeirarchyID = bootstrap AND Type = 4) ORDER BY `order`
+ERROR - 2014-03-07 16:43:28 --> Query error: Unknown column 'bootstrap' in 'where clause' - Invalid query: SELECT HeirarchyID FROM default_syllabus
+            WHERE Type = 4 AND ParentID = (SELECT ParentID FROM default_syllabus WHERE HeirarchyID = bootstrap AND Type = 4) ORDER BY `order`
+ERROR - 2014-03-07 16:44:51 --> Query error: Unknown column 'bootstrap' in 'where clause' - Invalid query: SELECT HeirarchyID FROM default_syllabus
+            WHERE Type = 4 AND ParentID = (SELECT ParentID FROM default_syllabus WHERE HeirarchyID = bootstrap AND Type = 4) ORDER BY `order`
+ERROR - 2014-03-07 16:44:54 --> Query error: Unknown column 'bootstrap' in 'where clause' - Invalid query: SELECT HeirarchyID FROM default_syllabus
+            WHERE Type = 4 AND ParentID = (SELECT ParentID FROM default_syllabus WHERE HeirarchyID = bootstrap AND Type = 4) ORDER BY `order`
+ERROR - 2014-03-07 16:44:55 --> Query error: Unknown column 'bootstrap' in 'where clause' - Invalid query: SELECT HeirarchyID FROM default_syllabus
+            WHERE Type = 4 AND ParentID = (SELECT ParentID FROM default_syllabus WHERE HeirarchyID = bootstrap AND Type = 4) ORDER BY `order`
+ERROR - 2014-03-07 16:45:04 --> Query error: Unknown column 'bootstrap' in 'where clause' - Invalid query: SELECT HeirarchyID FROM default_syllabus
+            WHERE Type = 4 AND ParentID = (SELECT ParentID FROM default_syllabus WHERE HeirarchyID = bootstrap AND Type = 4) ORDER BY `order`
+ERROR - 2014-03-07 16:46:57 --> Query error: Unknown column 'bootstrap' in 'where clause' - Invalid query: SELECT HeirarchyID FROM default_syllabus
+            WHERE Type = 4 AND ParentID = (SELECT ParentID FROM default_syllabus WHERE HeirarchyID = bootstrap AND Type = 4) ORDER BY `order`
+ERROR - 2014-03-07 16:46:59 --> Query error: Unknown column 'bootstrap' in 'where clause' - Invalid query: SELECT HeirarchyID FROM default_syllabus
+            WHERE Type = 4 AND ParentID = (SELECT ParentID FROM default_syllabus WHERE HeirarchyID = bootstrap AND Type = 4) ORDER BY `order`
+ERROR - 2014-03-07 16:47:56 --> Query error: Unknown column 'bootstrap' in 'where clause' - Invalid query: SELECT HeirarchyID FROM default_syllabus
+            WHERE Type = 4 AND ParentID = (SELECT ParentID FROM default_syllabus WHERE HeirarchyID = bootstrap AND Type = 4) ORDER BY `order`
+ERROR - 2014-03-07 16:48:56 --> Query error: Unknown column 'bootstrap' in 'where clause' - Invalid query: SELECT HeirarchyID FROM default_syllabus
+            WHERE Type = 4 AND ParentID = (SELECT ParentID FROM default_syllabus WHERE HeirarchyID = bootstrap AND Type = 4) ORDER BY `order`
+ERROR - 2014-03-07 16:49:39 --> Query error: Unknown column 'bootstrap' in 'where clause' - Invalid query: SELECT HeirarchyID FROM default_syllabus
+            WHERE Type = 4 AND ParentID = (SELECT ParentID FROM default_syllabus WHERE HeirarchyID = bootstrap AND Type = 4) ORDER BY `order`
+ERROR - 2014-03-07 16:49:40 --> Query error: Unknown column 'bootstrap' in 'where clause' - Invalid query: SELECT HeirarchyID FROM default_syllabus
+            WHERE Type = 4 AND ParentID = (SELECT ParentID FROM default_syllabus WHERE HeirarchyID = bootstrap AND Type = 4) ORDER BY `order`
