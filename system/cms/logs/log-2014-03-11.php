6340ace636fb15a0b151ceb74b9f8486122d3928
JMTee <janmarvint@gmail.com>
2014-03-12 15:21:00 +0800

    update
    update

---
+++ system/cms/logs/log-2014-03-11.php
@@ -0,0 +1,3 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2014-03-11 13:20:12 --> Page Missing: bootstrap/ico/favicon.png
---
+++ system/cms/logs/log-2014-03-12.php
@@ -0,0 +1,17 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2014-03-12 08:00:29 --> Page Missing: bootstrap/ico/favicon.png
+ERROR - 2014-03-12 08:00:32 --> Page Missing: bootstrap/ico/favicon.png
+ERROR - 2014-03-12 08:00:32 --> Page Missing: bootstrap/ico/favicon.png
+ERROR - 2014-03-12 08:00:33 --> Page Missing: bootstrap/ico/favicon.png
+ERROR - 2014-03-12 08:00:35 --> Page Missing: bootstrap/ico/favicon.png
+ERROR - 2014-03-12 15:08:26 --> Severity: Notice  --> Undefined offset: 0 C:\xampp\htdocs\eTESDA\system\cms\themes\pyrocms\views\admin\dashboard.php 180
+ERROR - 2014-03-12 08:12:18 --> Page Missing: learner/bootstrap/ico/favicon.png
+ERROR - 2014-03-12 08:12:35 --> Query error: Unknown column 'undefined' in 'where clause' - Invalid query: SELECT image, id ID, title, status
+            FROM default_course_courses WHERE id = undefined 
+ERROR - 2014-03-12 08:12:36 --> Query error: Unknown column 'undefined' in 'where clause' - Invalid query: SELECT image, id ID, title, status
+            FROM default_course_courses WHERE id = undefined 
+ERROR - 2014-03-12 08:12:41 --> Query error: Unknown column 'bootstrap' in 'where clause' - Invalid query: SELECT * FROM default_course_courses where id = bootstrap
+ERROR - 2014-03-12 08:12:58 --> Query error: Unknown column 'bootstrap' in 'where clause' - Invalid query: SELECT * FROM default_course_courses where id = bootstrap
+ERROR - 2014-03-12 08:13:08 --> Query error: Unknown column 'bootstrap' in 'where clause' - Invalid query: SELECT * FROM default_course_courses where id = bootstrap
+ERROR - 2014-03-12 08:15:06 --> Query error: Unknown column 'bootstrap' in 'where clause' - Invalid query: SELECT * FROM default_course_courses where id = bootstrap
